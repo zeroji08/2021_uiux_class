c137f76cd8d34f8b04bba78172d0fdeef7212309
zeroji08 <youngji6992@gmail.com>
2021-08-23 16:53:05 +0900

    1

---
+++ 20210817_from/from/welcome.php
@@ -0,0 +1,34 @@
+<<<<<<< HEAD
+<!DOCTYPE html>
+<html lang="ko">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?
+    echo $_GET["name"];
+    echo $_GET["email"];
+    ?>
+    <h2>안녕하세요.<? echo $_GET["name"]; ?> 님 만나서 반갑습니다.</h2>
+</body>
+=======
+<!DOCTYPE html>
+<html lang="ko">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?
+    echo $_GET["name"];
+    echo $_GET["email"];
+    ?>
+    <h2>안녕하세요.<? echo $_GET["name"]; ?> 님 만나서 반갑습니다.</h2>
+</body>
+>>>>>>> b2f3d887a757d967ec90c04728322bd27f3ebaa2
+</html>
